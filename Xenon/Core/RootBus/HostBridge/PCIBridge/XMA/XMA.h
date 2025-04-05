562eb47c95c23ca3a3eb2dfec97ac9cebcc0fb13
Xphalnos <164882787+Xphalnos@users.noreply.github.com>
2025-04-05 08:43:02 +0200

    [XMA] Adding Registers

--- Xenon/Core/RootBus/HostBridge/PCIBridge/XMA/XMA.h
+++ Xenon/Core/RootBus/HostBridge/PCIBridge/XMA/XMA.h
@@ -6,6 +6,20 @@
 
 #define XMA_DEV_SIZE 0x400
 
+// Taken from:
+// https://github.com/xenia-canary/xenia-canary/blob/canary_experimental/src/xenia/apu/xma_register_table.inc
+enum XE_XMA_REGISTERS {
+  CONTEXT_ARRAY_ADDRESS = 0x0600,
+  CURRENT_CONTEXT_INDEX = 0x0606,
+  NEXT_CONTEXT_INDEX = 0x0607,
+  UNKNOWN_610 = 0x0610,
+  UNKNOWN_620 = 0x0620,
+  KICK = 0x0650,
+  UNKNOWN_660 = 0x0660,
+  LOCK = 0x0690,
+  CLEAR = 0x06A0,
+};
+
 class XMA : public PCIDevice {
 public:
   XMA(const char *deviceName, u64 size);
