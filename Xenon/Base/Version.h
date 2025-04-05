7a84dd96470583756df4ad5004b29af42c844271
Michael Bishop <cleverca22@gmail.com>
2025-04-05 08:18:43 -0300

    Fix COMMIT_COUNT when building with nix (#60)

--- Xenon/Base/Version.h
+++ Xenon/Base/Version.h
@@ -4,11 +4,12 @@
 
 #include <fmt/format.h>
 
-#include "CommitCount.h"
-
 namespace Base {
 
-// Disable version display on Github artifacts because it's broken
-const std::string Version = fmt::format("Experimental v0.0.1{}", Base::commit_number != 1 ? fmt::format("-{}", Base::commit_number) : "");
+const std::string Version = "Experimental v0.0.1"
+#ifdef COMMIT_COUNT
+"-" COMMIT_COUNT
+#endif
+;
 
 }
