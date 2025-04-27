b752e083c9b05d7a3fd87b0d4f7f2996967f7b93
Vali <78507032+Vali0004@users.noreply.github.com>
2025-04-27 13:57:21 -0400

    [PPC] Remove testcase

--- Xenon/Core/XCPU/Interpreter/PPCInterpreter.cpp
+++ Xenon/Core/XCPU/Interpreter/PPCInterpreter.cpp
@@ -265,7 +265,6 @@ void PPCInterpreter::ppcInterpreterTrap(PPU_STATE *ppuState, u32 trapNumber) {
       LOG_XBOX(Xenon, "Assertion! Halting CPU... (Continuing will cause execution to resume as normal)");
       PPU *PPU = Xe_Main->xenonCPU->GetPPU(ppuState->ppuID);
       PPU->Halt(0, true, ppuState->ppuID, curThreadId);
-      //Xe_Main->xenonCPU->Halt(0, true, ppuState->ppuID, curThreadId);
 #else
       LOG_XBOX(Xenon, "Assertion! Continuing...");
 #endif
