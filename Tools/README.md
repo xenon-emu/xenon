a13af140c466b88c907ed9bd39da05fcfb4e3294
Vali <78507032+Vali0004@users.noreply.github.com>
2025-05-29 12:09:32 -0400

    [Tests] Add VPU test

--- Tools/README.md
+++ Tools/README.md
@@ -1,4 +1,7 @@
 # Miscellaneous tooling
+* AST: A test suite for the AMD Microcode AST
+* WinDBG_Test: A test suite for translating WinDBG to GDB
 * byteswap.cpp: A tool to byteswap values, then test against them
 * get_idx.cpp: A tool to get the register index based on a address
 * get_opcode.cpp: A tool to get the PM4 opcode from packet data
+* vpu_tets.cpp: A tool for quickly testing if a solution to a VPU instr will work
--- Tools/vpu_tests.cpp
+++ Tools/vpu_tests.cpp
@@ -3,9 +3,56 @@
 #include "Base/Logging/Log.h"
 #include "Base/Param.h"
 #include "Base/Types.h"
+#include "Base/Vector128.h"
+
+Base::Vector128 VR[128]{};
+struct DummyInstr {
+  u8 vsh = 0;
+  u8 va = 0;
+  u8 vb = 1;
+  u8 vc = 2;
+  u8 vd = 3;
+} _instr;
+#define VRi(x) VR[_instr.x]
+
+static inline u8 vsldoiHelper(u8 sh, Base::Vector128 vra, Base::Vector128 vrb) {
+  return (sh < 16) ? vra.bytes[sh] : vrb.bytes[sh & 15];
+}
 
 s32 ToolMain() {
-  
+  VRi(va).dword[0] = 0x00010203;
+  VRi(va).dword[1] = 0x04050607;
+  VRi(va).dword[2] = 0x08090A0B;
+  VRi(va).dword[3] = 0x0C0D0E0F;
+
+  VRi(vb).dword[0] = 0x10111213;
+  VRi(vb).dword[1] = 0x14151617;
+  VRi(vb).dword[2] = 0x18191A1B;
+  VRi(vb).dword[3] = 0x1C1D1E1F;
+
+  VRi(va).dword[0] = byteswap_be<u32>(VRi(va).dword[0]);
+  VRi(va).dword[1] = byteswap_be<u32>(VRi(va).dword[1]);
+  VRi(va).dword[2] = byteswap_be<u32>(VRi(va).dword[2]);
+  VRi(va).dword[3] = byteswap_be<u32>(VRi(va).dword[3]);
+
+  VRi(vb).dword[0] = byteswap_be<u32>(VRi(vb).dword[0]);
+  VRi(vb).dword[1] = byteswap_be<u32>(VRi(vb).dword[1]);
+  VRi(vb).dword[2] = byteswap_be<u32>(VRi(vb).dword[2]);
+  VRi(vb).dword[3] = byteswap_be<u32>(VRi(vb).dword[3]);
+
+  for (u8 idx = 0; idx < 16; idx++) {
+    VRi(vd).bytes[idx] = vsldoiHelper(_instr.vsh + idx, VRi(va), VRi(vb));
+  }
+
+  VRi(vd).dword[0] = byteswap_be<u32>(VRi(vd).dword[0]);
+  VRi(vd).dword[1] = byteswap_be<u32>(VRi(vd).dword[1]);
+  VRi(vd).dword[2] = byteswap_be<u32>(VRi(vd).dword[2]);
+  VRi(vd).dword[3] = byteswap_be<u32>(VRi(vd).dword[3]);
+
+  for (u8 i = 0; i != 4; ++i) {
+    LOG_INFO(Main, "VD{}: 0x{:08X}", i, VRi(vd).dword[i]);
+  }
+
   return 0;
 }
 
