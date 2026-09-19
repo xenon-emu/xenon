6d9878695a904fd5ced584a6671db78529ac5d5e
bitsh1ft3r <yeimik101@icloud.com>
2026-09-19 11:59:58 -0400

    [RAM]: Add Write Notification Callback and minor fixes.

--- Xenon/Core/RAM/RAM.cpp
+++ Xenon/Core/RAM/RAM.cpp
@@ -1,16 +1,19 @@
 /***************************************************************/
-/* Copyright 2025 Xenon Emulator Project. All rights reserved. */
+/* Copyright 2026 Xenon Emulator Project. All rights reserved. */
 /***************************************************************/
-
-#include "Base/Logging/Log.h"
-#include "Base/Hash.h"
 
 #include "RAM.h"
 
-RAM::RAM(const std::string &deviceName, u64 startAddress, std::string size, bool isSOCDevice) :
-  SystemDevice(deviceName, startAddress, startAddress + 0x20000000, isSOCDevice) {
+#include "Base/Hash.h"
+#include "Base/Logging/Log.h"
+
+// Contstructor
+RAM::RAM(const std::string& deviceName, u64 startAddress, std::string size, bool isSOCDevice)
+    : SystemDevice(deviceName, startAddress, startAddress + 0x20000000, isSOCDevice) {
+
+  // Parse RAM size.
   char firstChar = '\0';
-  for (auto &c : size) {
+  for (auto& c : size) {
     if (!std::isdigit(c)) {
       firstChar = c;
       break;
@@ -26,119 +29,128 @@ RAM::RAM(const std::string &deviceName, u64 startAddress, std::string size, bool
       std::string unit = size.substr(charPos);
       f64 ramSizeF = std::stod(number.c_str());
       f64 unitF = 0;
-      //536870912 = 0x20_000_000
-      //1073741824 = 0x40_000_000
+      // 536870912 = 0x20_000_000
+      // 1073741824 = 0x40_000_000
       switch (Base::JoaatStringHash(unit)) {
-      case "B"_jLower:
-        unitF = 1;
-        break;
-      case "KB"_jLower:
-        if (ramSizeF < 536870.912) {
-          ramSizeF = 536870.912;
-          LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 536870.912KB", ramSizeF);
-        }
-        unitF = static_cast<f64>(1_KB);
-        break;
-      case "KiB"_jLower:
-        if (ramSizeF < 524288) {
-          ramSizeF = 524288;
-          LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 524288KiB", ramSizeF);
-        }
-        unitF = static_cast<f64>(1_KiB);
-        break;
-      case "MB"_jLower:
-        if (ramSizeF < 536.870912) {
-          ramSizeF = 536.870912;
-          LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 536.870912MB", ramSizeF);
-        }
-        unitF = static_cast<f64>(1_MB);
-        break;
-      case "MiB"_jLower:
-        if (ramSizeF < 512) {
+        case "B"_jLower: unitF = 1; break;
+        case "KB"_jLower:
+          if (ramSizeF < 536870.912) {
+            ramSizeF = 536870.912;
+            LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 536870.912KB", ramSizeF);
+          }
+          unitF = static_cast<f64>(1_KB);
+          break;
+        case "KiB"_jLower:
+          if (ramSizeF < 524288) {
+            ramSizeF = 524288;
+            LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 524288KiB", ramSizeF);
+          }
+          unitF = static_cast<f64>(1_KiB);
+          break;
+        case "MB"_jLower:
+          if (ramSizeF < 536.870912) {
+            ramSizeF = 536.870912;
+            LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 536.870912MB", ramSizeF);
+          }
+          unitF = static_cast<f64>(1_MB);
+          break;
+        case "MiB"_jLower:
+          if (ramSizeF < 512) {
+            ramSizeF = 512;
+            LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 512MiB", ramSizeF);
+          }
+          unitF = static_cast<f64>(1_MiB);
+          break;
+        case "GB"_jLower:
+          if (ramSizeF < 1.073741824) {
+            ramSizeF = 1.073741824;
+            LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 1.073741824GB", ramSizeF);
+          }
+          unitF = static_cast<f64>(1_GB);
+          break;
+        case "GiB"_jLower:
+          if (ramSizeF < 1) {
+            ramSizeF = 1;
+            LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 1GiB", ramSizeF);
+          }
+          unitF = static_cast<f64>(1_GiB);
+          break;
+        default:
           ramSizeF = 512;
-          LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 512MiB", ramSizeF);
-        }
-        unitF = static_cast<f64>(1_MiB);
-        break;
-      case "GB"_jLower:
-        if (ramSizeF < 1.073741824) {
-          ramSizeF = 1.073741824;
-          LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 1.073741824GB", ramSizeF);
-        }
-        unitF = static_cast<f64>(1_GB);
-        break;
-      case "GiB"_jLower:
-        if (ramSizeF < 1) {
-          ramSizeF = 1;
-          LOG_ERROR(System, "Invalid RAM size '{}'! Defaulting to 1GiB", ramSizeF);
-        }
-        unitF = static_cast<f64>(1_GiB);
-        break;
-      default:
-        ramSizeF = 512;
-        unitF = static_cast<f64>(1_MiB);
-        LOG_ERROR(System, "Invalid RAM Unit '{}'! Defaulting to 512MiB", unit);
-        break;
+          unitF = static_cast<f64>(1_MiB);
+          LOG_ERROR(System, "Invalid RAM Unit '{}'! Defaulting to 512MiB", unit);
+          break;
       }
       ramSize = static_cast<u64>(std::round(ramSizeF * unitF));
       LOG_INFO(Xenon, "RAM Size: {}", size);
     }
   }
+
+  // Update the end address of the RAM device based on the new size.
   UpdateEndAddress(GetStartAddress() + ramSize);
+
+  // Allocate and intialize RAM.
   ramData = std::make_unique<STRIP_UNIQUE_ARR(ramData)>(ramSize);
   if (!ramData.get()) {
     LOG_CRITICAL(System, "RAM failed to allocate! This is really bad!");
     Base::SystemPause();
   } else {
-    memset(ramData.get(), 0xCD, ramSize);
+    ramBasePtr = ramData.get();
+    memset(ramBasePtr, 0xCD, ramSize);
   }
 }
-RAM::~RAM() {
-  ramData.reset();
-}
 
+// Destructor
+RAM::~RAM() { ramData.reset(); }
+
+// Resets the RAM to default state.
+// NOTE: Real hardware preserves RAM contents across reboots.
 void RAM::Reset() {
-  if (!ramData.get()) {
-    ramData = std::make_unique<STRIP_UNIQUE_ARR(ramData)>(ramSize);
-    memset(ramData.get(), 0xCD, ramSize);
-  } else {
-    memset(ramData.get(), 0xCD, ramSize);
-  }
+  if (!ramData.get()) { ramData = std::make_unique<STRIP_UNIQUE_ARR(ramData)>(ramSize); }
+  ramBasePtr = ramData.get();
+  memset(ramBasePtr, 0xCD, ramSize);
 }
 
+// Resizes the RAM to a new size.
 void RAM::Resize(u64 size) {
   ramSize = size;
-  if (!ramData.get()) {
-    ramData = std::make_unique<STRIP_UNIQUE_ARR(ramData)>(ramSize);
-  }
+  if (!ramData.get()) { ramData = std::make_unique<STRIP_UNIQUE_ARR(ramData)>(ramSize); }
+  ramBasePtr = ramData.get();
 }
 
-void RAM::Read(u64 readAddress, u8 *data, u64 size) {
+// MMIO Read.
+void RAM::Read(u64 readAddress, u8* data, u64 size) {
   const u64 offset = static_cast<u32>(readAddress - RAM_START_ADDR);
-  memcpy(data, ramData.get() + offset, size);
-  if (false)
-    LOG_TRACE(Xenon, "Reading {:#08x} bytes from {:#08x}", size, readAddress);
+  memcpy(data, ramBasePtr + offset, size);
+  if (false) LOG_TRACE(Xenon, "Reading {:#08x} bytes from {:#08x}", size, readAddress);
 }
 
-void RAM::Write(u64 writeAddress, const u8 *data, u64 size) {
+// MMIO Write.
+void RAM::Write(u64 writeAddress, const u8* data, u64 size) {
   const u32 offset = static_cast<u32>(writeAddress - RAM_START_ADDR);
-  memcpy(ramData.get() + offset, data, size);
-  if (false)
-    LOG_TRACE(Xenon, "Writing {:#08x} bytes to {:#08x}", size, writeAddress);
+  memcpy(ramBasePtr + offset, data, size);
+  if (false) LOG_TRACE(Xenon, "Writing {:#08x} bytes to {:#08x}", size, writeAddress);
+
+  // Notify observers about the physical write.
+  if (writeNotifyCallback) { writeNotifyCallback(offset, static_cast<u32>(size)); }
 }
 
+// MMIO MemSet.
 void RAM::MemSet(u64 writeAddress, s32 data, u64 size) {
   const u32 offset = static_cast<u32>(writeAddress - RAM_START_ADDR);
-  memset(ramData.get() + offset, data, size);
-  if (false)
-    LOG_TRACE(Xenon, "Setting {:#08x} to {:#02x} for {:#08x} bytes", writeAddress, data, size);
+  memset(ramBasePtr + offset, data, size);
+  if (false) LOG_TRACE(Xenon, "Setting {:#08x} to {:#02x} for {:#08x} bytes", writeAddress, data, size);
+
+  // Notify observers about the physical write.
+  if (writeNotifyCallback) { writeNotifyCallback(offset, static_cast<u32>(size)); }
 }
 
-u8 *RAM::GetPointerToAddress(u32 address) {
+// Returns a pointer for a specific offset from RAM Base or nullptr for OOB.
+u8* RAM::GetPointerToAddress(u32 address) {
   const u64 offset = static_cast<u32>(address - RAM_START_ADDR);
   if (offset > ramSize) {
     LOG_CRITICAL(System, "[RAM]: GetPointerToAddress out of bounds access at address {:#x}!", address);
     return nullptr;
   }
-  return ramData.get() + offset;
+  return ramBasePtr + offset;
 }
--- Xenon/Core/RAM/RAM.h
+++ Xenon/Core/RAM/RAM.h
@@ -1,31 +1,53 @@
 /***************************************************************/
-/* Copyright 2025 Xenon Emulator Project. All rights reserved. */
+/* Copyright 2026 Xenon Emulator Project. All rights reserved. */
 /***************************************************************/
 
 #pragma once
 
-#include <memory>
-
 #include "Base/SystemDevice.h"
+
+#include <functional>
+#include <memory>
 
 #define RAM_START_ADDR 0
 
+// Random Access Memory
 class RAM : public SystemDevice {
 public:
-  RAM(const std::string &deviceName, u64 startAddress, std::string size,
-    bool isSOCDevice);
+  RAM(const std::string& deviceName, u64 startAddress, std::string size, bool isSOCDevice);
   ~RAM();
   void Reset();
   void Resize(u64 size);
-  void Read(u64 readAddress, u8 *data, u64 size) override;
-  void Write(u64 writeAddress, const u8 *data, u64 size) override;
+  void Read(u64 readAddress, u8* data, u64 size) override;
+  void Write(u64 writeAddress, const u8* data, u64 size) override;
   void MemSet(u64 writeAddress, s32 data, u64 size) override;
 
-  u8 *GetPointerToAddress(u32 address);
-  u64 GetSize() {
-    return ramSize;
+  // Returns a host pointer to a given address belonging to RAM.
+  u8* GetPointerToAddress(u32 address);
+  // Returns a pointer to the RAM base in host memory.
+  u8* GetRamBase() { return ramBasePtr; }
+  // Returns current RAM size.
+  u64 GetSize() { return ramSize; }
+
+  // Physical memory write notification callback
+  // Called after every write to RAM so that observers (such as the GPU) can track which pages have been modified by the
+  // CPU. NOTE: Callers must use a page system to track writes or at least do a full Cache Line invalidation by aligning
+  // the address to 128 bytes.
+  using WriteNotifyCallback = std::function<void(u32 physical_address, u32 length)>;
+  void SetWriteNotifyCallback(WriteNotifyCallback callback) { writeNotifyCallback = std::move(callback); }
+
+  // Used by accessors to notify a 'direct' write to RAM has ocurred without going through the MMIO interface.
+  void NotifyWrite(u32 physicalAddress, u32 byteCount) {
+    if (writeNotifyCallback) { writeNotifyCallback(physicalAddress, byteCount); }
   }
+
 private:
+  // Current RAM size in bytes.
   u64 ramSize = 0;
+  // Current RAM base pointer.
+  u8* ramBasePtr = nullptr;
+  // Dynamic allocation of RAM data.
   std::unique_ptr<u8[]> ramData{};
+  // Write notification Callback for observers to track writes to RAM.
+  WriteNotifyCallback writeNotifyCallback;
 };
